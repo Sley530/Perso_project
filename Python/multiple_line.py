31719164e146b0e709c63ac8af6bf9dd9a2a9ca3
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-11 22:18:41 -0500

    Update

--- Python/multiple_line.py
+++ Python/multiple_line.py
@@ -3,9 +3,10 @@ import time
 
 time.sleep(4) # Time in seconds before to take action
 
+# Beginning of loop
 count = 0
 
-while count <= 500:
+while count <= 500: # Number of lines to display 
     pyautogui.typewrite("enter your message here" + str (count))
     pyautogui.press("enter")
     count += 1
